67aae4d19e8d9c35bd16db599da7ca64b5e1d417
zulfabintangkejora-creator <zulfabintangkejora@gmail.com>
2026-09-15 15:08:06 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// Konfigurasi Firebase untuk game Family 100 – Berlomba-lomba dalam Kebaikan
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAYiF-IdjM-hD_bfwNOewWIgxbOwrLla-0",
+  authDomain: "family100-kebaikan.firebaseapp.com",
+  databaseURL: "https://family100-kebaikan-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "family100-kebaikan",
+  storageBucket: "family100-kebaikan.firebasestorage.app",
+  messagingSenderId: "995189747968",
+  appId: "1:995189747968:web:15c40aa1bfaf38a00043e5"
+};
